fe60b636420393a8dc56f5cc55428daa4d75fc48
nicjo <nicjorgensen@yahoo.ca>
2016-03-09 17:11:08 +0000

    list page showing titles and linking to each 'page'

--- src/js/app.js
+++ src/js/app.js
@@ -64,6 +64,129 @@ var App = React.createClass({
   }
 });
 
+// Placeholder data for user generated paths. they include a title + the coordinates for each marker/crumb
+var pathData = [
+    {
+      id: 0,
+      title: "First Map",
+      points: [
+        {lat: 45.5034125, lng: -73.5694394},
+        {lat: 45.5034501, lng: -73.5695359},
+        {lat: 45.5028636, lng: -73.5701233},
+        {lat: 45.5032283, lng: -73.5708851},
+        {lat: 45.5028786, lng: -73.5713142},
+        {lat: 45.5029012, lng: -73.5714215},
+        {lat: 45.5025402, lng: -73.5717890},
+        {lat: 45.5025647, lng: -73.5718238}
+      ]
+    },
+    {
+      id: 1,
+      title: "Sweet MTL map",
+      points: [
+        {lat: 45.4952833, lng: -73.5698068},
+        {lat: 45.4955240, lng: -73.5694420},
+        {lat: 45.4954225, lng: -73.5691470},
+        {lat: 45.4961670, lng: -73.5681465},
+        {lat: 45.4967611, lng: -73.5673955},
+        {lat: 45.4974624, lng: -73.5687125},
+        {lat: 45.4975940, lng: -73.5685301},
+        {lat: 45.4978572, lng: -73.5672963},
+        {lat: 45.4982859, lng: -73.5669529},
+        {lat: 45.4984513, lng: -73.5669208},
+        {lat: 45.4991206, lng: -73.5663199},
+        {lat: 45.4993688, lng: -73.5666847},
+        {lat: 45.5000756, lng: -73.5676074},
+        {lat: 45.5004366, lng: -73.5683692},
+        {lat: 45.5006171, lng: -73.5689056},
+        {lat: 45.5007261, lng: -73.5690880},
+        {lat: 45.5009385, lng: -73.5689244},
+        {lat: 45.5012393, lng: -73.5694528},
+        {lat: 45.5013446, lng: -73.5694286},
+        {lat: 45.5015570, lng: -73.5697666},
+        {lat: 45.5017601, lng: -73.5695815}
+      ]
+    },
+    {
+      id: 2,
+      title: "Secret Party Location",
+      points: [
+        {lat: 45.4953341, lng: -73.5628062},
+        {lat: 45.4940255, lng: -73.5593891},
+        {lat: 45.4957553, lng: -73.5581070},
+        {lat: 45.4955936, lng: -73.5576135}
+      ]
+    },
+    {
+      id: 3,
+      title: "Tresure Trail ;)",
+      points: [
+        {lat: 45.4987371, lng: -73.5528338},
+        {lat: 45.4963757, lng: -73.5530055},
+        {lat: 45.4952777, lng: -73.5509777},
+        {lat: 45.4910134, lng: -73.5515785},
+        {lat: 45.4909006, lng: -73.5502696},
+        {lat: 45.4905245, lng: -73.5501945},
+        {lat: 45.4894565, lng: -73.5440576},
+        {lat: 45.4930365, lng: -73.5450876},
+        {lat: 45.5014518, lng: -73.5441864},
+        {lat: 45.5013239, lng: -73.5428989},
+        {lat: 45.5003313, lng: -73.5428560},
+      ]
+    },
+    {
+      id: 4,
+      title: "Grandma's house",
+      points: [
+        {lat: 45.5226873, lng: -73.6017841},
+        {lat: 45.5252655, lng: -73.5994291},
+        {lat: 45.5232811, lng: -73.5950518},
+        {lat: 45.5229541, lng: -73.5953414}
+      ]
+    }
+  ]
+  
+
+// var listicle = pathData.map(function(obj){
+//                       var rObj = {};
+//                       rObj = obj.title;
+//                       return React.DOM.li({className: 'listItem'}, null, rObj)
+//                     });
+
+
+// var listicle = pathData.map(function(obj){
+//                       var link = "/view/" + obj.id
+//                       return (<li key={obj.id} className="listItem"><a href={link}>{obj.title}</a></li>)
+//                     });
+
+var PathList = React.createClass ({
+  
+  render: function(Data) {
+    return (
+      <div>
+        <p>Derpington</p>
+        <ul className="path-list">
+          {pathData.map(item => (
+              <li key={item.id} className="listItem"><Link to={`/view/${item.id}`}>{item.title}</Link></li>
+            ))}
+        </ul>
+      </div>
+      );
+  }
+})
+
+
+var PathView = React.createClass({
+  
+  render: function() {
+    return <p>Path View</p>
+  }
+  
+  
+})
+
+
+
 var Geolocation = React.createClass ({
 
   getInitialState: function() {
@@ -288,20 +411,8 @@ var Home = React.createClass({
     return <p>HOME</p>
   }
 })
-var PathList = React.createClass({
-  
-  render: function() {
-    return <p>Path List</p>
-  }
-})
-var PathView = React.createClass({
-  
-  render: function() {
-    return <p>Path View</p>
-  }
-  
-  
-})
+
+
 
 
 
