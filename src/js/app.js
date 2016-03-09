290a90ec69ac52cbf1af27c0e4c14eea34463f69
Hanna Nicol <hannanicol@gmail.com>
2016-03-09 19:08:55 +0000

    can drop and pick up crumbs click events in componant props

--- src/js/app.js
+++ src/js/app.js
@@ -184,234 +184,251 @@ var Geolocation = React.createClass({
         }
       });
 
-    var SimpleMap = React.createClass({
+var SimpleMap = React.createClass({
 
-      render: function() {
-        // var that = this;
-        return (
-          <section style={{height: "100vh"}}>
-      <GoogleMapLoader
-        containerElement={
-          <div
-            {...this.props}
-            style={{
-              height: "100%",
-            }}
-          />
+  render: function() {
+    // var that = this;
+    return (
+      <section style={{height: "100vh"}}>
+  <GoogleMapLoader
+    containerElement={
+      <div
+        {...this.props}
+        style={{
+          height: "100%",
+        }}
+      />
+    }
+    googleMapElement={
+      <GoogleMap
+      
+        ref={(map) => console.log(map)}
+        defaultZoom={12}
+        defaultCenter={{lat: 45.5088400, lng: -73.5878100}}
+        // onClick={::this.handleMapClick}>
+        >
+        
+        {/*{that.state.crumbs.map((crumb, index) => {
+          return (
+            <Marker
+              {...crumb}
+              onRightclick={that.handleCrumbRightclick.bind(that, index)} />
+          );
+        })}*/}
+      </GoogleMap>
+    }
+  />
+</section>
+    );
+  }
+});
+
+var PlotMap = React.createClass({
+  getInitialState: function() {
+    return {
+      crumbs: [{
+        position: {
+          lat: 45.5088400,
+          lng: -73.5878100,
+        },
+        key: `Montreal`,
+        defaultAnimation: 2,
+      }]
+    };
+  },
+
+  componentDidMount: function() {
+    if (!canUseDOM) {
+      return;
+    }
+    window.addEventListener(`resize`, this.handleWindowResize);
+  },
+  componentWillUnmount: function() {
+    if (!canUseDOM) {
+      return;
+    }
+    window.removeEventListener('resize', this.handleWindowResize);
+  },
+
+  render: function() {
+    return ( < GoogleMapLoader containerElement = { < div {...this.props
         }
-        googleMapElement={
-          <GoogleMap
-          
-            ref={(map) => console.log(map)}
-            defaultZoom={12}
-            defaultCenter={{lat: 45.5088400, lng: -73.5878100}}
-            // onClick={::this.handleMapClick}>
-            >
+        style = {
+          {
+            height: `100vh`,
+          }
+        }
+        />
+      }
+      googleMapElement = {
+        <GoogleMap
+            ref={(map) => (this._googleMapComponent = map)}
+            defaultZoom={16}
+            defaultCenter={{ lat: 45.5088400, lng: -73.5878100 }}
+            onClick={this.props.onClick}
             
-            {/*{that.state.crumbs.map((crumb, index) => {
+          >
+            {this.props.crumbs.map((crumb, index) => {
               return (
                 <Marker
                   {...crumb}
-                  onRightclick={that.handleCrumbRightclick.bind(that, index)} />
+                  onRightclick={this.props.onRightClick.bind(null, index)} //.bind(this, index) now passed to onRightclick call
+                />
               );
-            })}*/}
+            })}
           </GoogleMap>
-        }
+      }
       />
-    </section>
-        );
-      }
-    });
-
-    var PlotMap = React.createClass({
-      getInitialState: function() {
-        return {
-          crumbs: [{
-            position: {
-              lat: 25.0112183,
-              lng: 121.5206757000001,
-            },
-            key: `Montreal`,
-            defaultAnimation: 2,
-          }]
-        };
-      },
-
-      componentDidMount: function() {
-        if (!canUseDOM) {
-          return;
-        }
-        window.addEventListener(`resize`, this.handleWindowResize);
-      },
-      componentWillUnmount: function() {
-        if (!canUseDOM) {
-          return;
-        }
-        window.removeEventListener('resize', this.handleWindowResize);
-      },
-
-      handleMapClick: function(e) {
-        let {
-          crumbs
-        } = this.state;
-        crumbs = update(crumbs, {
-          $push: [{
-            position: {
-              lat: e.latLng.lat(),
-              lng: e.latLng.lng()
-            },
-            key: Date.now()
-          }, ],
-        });
-        this.setState({
-          crumbs
-        });
-
-        /*if (crumbs.length === 3) {
-          this.props.toast(
-            `Right click on the crumb to remove it`,
-            `Also check the code!`
-          );
-        }*/
-      },
-      handleCrumbRightclick: function(i, e) {
-        let {
-          crumbs
-        } = this.state;
-        crumbs = update(crumbs, {
-          $splice: [
-            [i, 1],
-          ],
-        });
-        this.setState({
-          crumbs
-        });
-      },
-      render: function() {
-        return ( < GoogleMapLoader containerElement = { < div {...this.props
-            }
-            style = {
-              {
-                height: `100vh`,
-              }
-            }
-            />
-          }
-          googleMapElement = {
-            <GoogleMap
-                ref={(map) => (this._googleMapComponent = map) && console.log(map.getZoom())}
-                defaultZoom={16}
-                defaultCenter={{ lat: 45.5088400, lng: -73.5878100 }}
-                onClick={this.handleMapClick}
-                
-              >
-                {this.state.crumbs.map((crumb, index) => {
-                  return (
-                    <Marker
-                      {...crumb}
-                      onRightclick={this.handleCrumbRightclick.bind(this, index)}
-                    />
-                  );
-                })}
-              </GoogleMap>
-          }
-          />
-        );
-      }
-    });
-
-    var Home = React.createClass({
-
-      render: function() {
-        return <p>HOME</p>
-      }
-    });
-    var PathList = React.createClass({
-
-      render: function() {
-        return <p>Path List</p>
-      }
-    });
-    var PathView = React.createClass({
-
-      render: function() {
-        return <p>Path View</p>
-      }
-    });
-
-    var Title = React.createClass({
-
-      render: function() {
-        return (
-          <form action="create">
-    <input type="text" name="pathTitle" ref= "title" placeholder="Name your path"/>
-    </form>
-        );
-      }
-    });
-
-    var Create = React.createClass({
-
-      handleClick: function(e) {
-        e.preventDefault()
-        var path = {
-          title: this.refs.title,
-          points: []
-        }
-        console.log(path);
-        browserHistory.push("/list");
-
-      },
-
-      render: function() {
-        return (
-          <button onClick={this.handleClick}>Create</button>
-        );
-      }
-    });
-
-    var PlotPage = React.createClass({
-
-      render: function() {
-        return (
-          <div>
-           <Title/>
-           <PlotMap/>
-           <Create/>
-         </div>
-        )
-      }
-    });
-
-    // not found "page"
-    var NotFound = React.createClass({
-      render: function() {
-        return (
-          <div>Not Found!</div>
-        );
-      }
-    });
-    /*
-    The routes. This section says:
-      - If the route starts with /, load the App component
-      - If the route IS /, load the Home component INSIDE App as this.props.children
-      - If the route is /about, load the About component INSIDE App as this.props.children
-      - If the route is /team, load the Team component INSIDE App as this.props.children
-      - If the route is /about, load the About component INSIDE App as this.props.children
-      - If the route is anything else, load the NotFound component INSIDE App as this.props.children
-    The whole process lets us create **complex, nested user interfaces** with minimal effort,
-    by simply nesting `Route` components.
-    */
-    var routes = (
-      <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Home}/>
-      <Route path="create" component={PlotPage}/>
-      <Route path="list" component={PathList}/>
-      <Route path="view/:id" component={PathView}/>
-      <Route path="*" component={NotFound}/>
-    </Route>
-  </Router>
     );
-    // If this line of code is not here, nothing gets displayed!
-    ReactDOM.render(routes, document.querySelector('#app'));
+  }
+});
+
+var Home = React.createClass({
+
+  render: function() {
+    return <p>HOME</p>
+  }
+});
+var PathList = React.createClass({
+
+  render: function() {
+    return <p>Path List</p>
+  }
+});
+var PathView = React.createClass({
+
+  render: function() {
+    return <p>Path View</p>
+  }
+});
+
+var PlotPage = React.createClass({
+  // var title = 'untitled';
+  // var bcrumbs = [];
+  
+  getInitialState: function() {
+   return {
+     title: 'untitled',
+     bcrumbs: [],
+     crumbs: []
+   }
+  },
+  handleKey: function(e) {
+    var formtitle = 'untitled'
+    if (e.target.value.length>0) {
+      formtitle=e.target.value
+    };
+    this.setState({title: formtitle});
+  },
+  handleClick: function(e) {
+    e.preventDefault()
+    var path = {
+      title: this.state.title,
+      points: this.state.crumbs
+    }
+    console.log(path);
+    this.setState({
+      crumbs: [],
+      title: "untitled"
+    })
+    
+    browserHistory.push("/list");
+
+  },
+  handleMapClick: function(e) {
+    
+    if(e.latLng){
+      
+      let {crumbs} = this.state ///ES6 destructuring === var crumbs = this.state.crumbs
+    //in ES5->
+    crumbs.push({position: {lat: e.latLng.lat(), lng: e.latLng.lng()}, key: Date.now()})
+    /*crumbs = update(crumbs, {
+      $push: [{
+        position: {
+          lat: e.latLng.lat(),
+          lng: e.latLng.lng()
+        },
+        key: Date.now()
+      }, ],
+    });*/
+    
+    console.log(crumbs)
+    this.setState({
+      crumbs: crumbs
+    });
+    // this.bcrumbs = {crumbs};
+    /*if (crumbs.length === 3) {
+      this.props.toast(
+        `Right click on the crumb to remove it`,
+        `Also check the code!`
+      );
+    }*/
+    }
+    
+  },
+  handleCrumbRightClick: function(i) {
+    
+    console.log(i);
+    
+    var crumbs = this.state.crumbs;
+    crumbs.splice(i,1);
+    this.setState({crumbs:crumbs});
+    /*let {
+      crumbs
+    } = this.state;*/
+    /*crumbs = update(crumbs, {
+      $splice: [
+        [i, 1],
+      ],
+    });
+    this.setState({
+      crumbs
+    });*/
+  },
+  render: function() {
+    var formtitle = this.state.title;
+    return (
+      <div>
+        <form action="create">
+          <p>{formtitle}</p>
+          <input onKeyUp={this.handleKey} type="text" name="pathTitle" ref= "title" placeholder="Name your path"/>
+        </form>
+       <PlotMap crumbs={this.state.crumbs} onClick= {this.handleMapClick} onRightClick= {this.handleCrumbRightClick}/>
+       <button onClick={this.handleClick}>Create</button>
+     </div>
+    )
+  }
+});
+
+// not found "page"
+var NotFound = React.createClass({
+  render: function() {
+    return (
+      <div>Not Found!</div>
+    );
+  }
+});
+/*
+The routes. This section says:
+  - If the route starts with /, load the App component
+  - If the route IS /, load the Home component INSIDE App as this.props.children
+  - If the route is /about, load the About component INSIDE App as this.props.children
+  - If the route is /team, load the Team component INSIDE App as this.props.children
+  - If the route is /about, load the About component INSIDE App as this.props.children
+  - If the route is anything else, load the NotFound component INSIDE App as this.props.children
+The whole process lets us create **complex, nested user interfaces** with minimal effort,
+by simply nesting `Route` components.
+*/
+var routes = (
+  <Router history={browserHistory}>
+<Route path="/" component={App}>
+  <IndexRoute component={Home}/>
+  <Route path="create" component={PlotPage}/>
+  <Route path="list" component={PathList}/>
+  <Route path="view/:id" component={PathView}/>
+  <Route path="*" component={NotFound}/>
+</Route>
+</Router>
+);
+// If this line of code is not here, nothing gets displayed!
+ReactDOM.render(routes, document.querySelector('#app'));
