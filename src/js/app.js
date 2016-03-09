ce913656d3376854c109573c51c2936c8c72258f
Hanna Nicol <hannanicol@gmail.com>
2016-03-09 00:39:38 +0000

    plot map onClick

--- src/js/app.js
+++ src/js/app.js
@@ -8,18 +8,34 @@ var Link = ReactRouter.Link;
 var IndexRoute = ReactRouter.IndexRoute;
 var browserHistory = ReactRouter.browserHistory;
 
-import { default as update } from "react-addons-update";
-import { default as _ } from "lodash";
+import {
+  default as update
+}
+from "react-addons-update";
+import {
+  default as _
+}
+from "lodash";
 
-import {GoogleMapLoader, GoogleMap, Marker, Circle, InfoWindow} from "react-google-maps";
+import {
+  GoogleMapLoader, GoogleMap, Marker, Circle, InfoWindow
+}
+from "react-google-maps";
 
 import {
   default as canUseDOM,
-} from "can-use-dom";
+}
+from "can-use-dom";
 
-import { default as raf } from "raf";
+import {
+  default as raf
+}
+from "raf";
 
-import { triggerEvent } from "react-google-maps/lib/utils";
+import {
+  triggerEvent
+}
+from "react-google-maps/lib/utils";
 
 var geolocation = (
   canUseDOM && navigator.geolocation || {
@@ -64,99 +80,116 @@ var App = React.createClass({
   }
 });
 
-var Geolocation = React.createClass ({
+var Geolocation = React.createClass({
 
-  getInitialState: function() {
-    return {
-    center: null,
-    content: null,
-    radius: 6000,
-    crumbs: [],
-    }
-  },
-  
-  addCrumb: function(crumb) {
-    var crumbs = this.state.crumbs;
-    crumbs.push(crumb);
-    this.forceUpdate();
-    
-  },
-  
-  /*touchMap: function(e) {
-    
-  },*/
-  
-  componentDidMount: function() {
-    geolocation.getCurrentPosition((position) => {
-      this.setState({
-        center: {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        },
-        content: `Location found using HTML5.`,
-      });
-
-      const tick = () => {
-        this.setState({ radius: Math.max(this.state.radius - 20, 0) });
-
-        if (this.state.radius > 100) {
-          raf(tick);
+      getInitialState: function() {
+        return {
+          center: null,
+          content: null,
+          radius: 6000,
+          crumbs: [],
         }
-      };
-      raf(tick);
-    }, (reason) => {
-      this.setState({
-        center: {
-          lat: 60,
-          lng: 105,
-        },
-        content: `Error: The Geolocation service failed (${ reason }).`,
-      });
-    });
-  },
-  render: function() {
-    
-    const { center, content, radius } = this.state;
-    let contents = [];
+      },
 
-    if (center) {
-      contents = contents.concat([
-        (<InfoWindow key="info" position={center} content={content} />),
-        (<Circle key="circle" center={center} radius={radius} options={{
-          fillColor: `red`,
-          fillOpacity: 0.20,
-          strokeColor: `red`,
-          strokeOpacity: 1,
-          strokeWeight: 1,
-        }}
-        />),
-      ]);
-    }
+      addCrumb: function(crumb) {
+        var crumbs = this.state.crumbs;
+        crumbs.push(crumb);
+        this.forceUpdate();
 
-    return (
-        <GoogleMap
-          containerProps={{
-            ...this.props,  //requires stage-2 preset
-            style: {
-              height: `100vh`,
+      },
+
+      /*touchMap: function(e) {
+        
+      },*/
+
+      componentDidMount: function() {
+        geolocation.getCurrentPosition((position) => {
+          this.setState({
+            center: {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
             },
-          }}
-          defaultZoom={17}
-          center={center}
-          onClick={this.addCrumb}
-        >
-          {contents}
-        </GoogleMap>
-    );
-  }
-});
+            content: `Location found using HTML5.`,
+          });
 
-var SimpleMap = React.createClass({
-  
-  render: function() {
-    // var that = this;
-      return (
-    <section style={{height: "100vh"}}>
+          const tick = () => {
+            this.setState({
+              radius: Math.max(this.state.radius - 20, 0)
+            });
+
+            if (this.state.radius > 100) {
+              raf(tick);
+            }
+          };
+          raf(tick);
+        }, (reason) => {
+          this.setState({
+            center: {
+              lat: 60,
+              lng: 105,
+            },
+            content: `Error: The Geolocation service failed (${ reason }).`,
+          });
+        });
+      },
+      render: function() {
+
+        const {
+          center, content, radius
+        } = this.state;
+        let contents = [];
+
+        if (center) {
+          contents = contents.concat([
+              (<InfoWindow key="info" position={center} content={content} />), ( < Circle key = "circle"
+                center = {
+                  center
+                }
+                radius = {
+                  radius
+                }
+                options = {
+                  {
+                    fillColor: `red`,
+                    fillOpacity: 0.20,
+                    strokeColor: `red`,
+                    strokeOpacity: 1,
+                    strokeWeight: 1,
+                  }
+                }
+                />),
+              ]);
+          }
+
+          return ( < GoogleMap containerProps = {
+              {
+                ...this.props, //requires stage-2 preset
+                  style: {
+                    height: `100vh`,
+                  },
+              }
+            }
+            defaultZoom = {
+              17
+            }
+            center = {
+              center
+            }
+            onClick = {
+              this.addCrumb
+            } > {
+              contents
+            } < /GoogleMap>
+          );
+        }
+      });
+
+    var SimpleMap = React.createClass({
+
+      render: function() {
+        // var that = this;
+        return (
+          <section style={{height: "100vh"}}>
       <GoogleMapLoader
         containerElement={
           <div
@@ -175,166 +208,210 @@ var SimpleMap = React.createClass({
             // onClick={::this.handleMapClick}>
             >
             
-            {/*{that.state.markers.map((marker, index) => {
+            {/*{that.state.crumbs.map((crumb, index) => {
               return (
                 <Marker
-                  {...marker}
-                  onRightclick={that.handleMarkerRightclick.bind(that, index)} />
+                  {...crumb}
+                  onRightclick={that.handleCrumbRightclick.bind(that, index)} />
               );
             })}*/}
           </GoogleMap>
         }
       />
     </section>
-  );
-  }  
-  
-})
+        );
+      }
+    });
 
-var PlotMap = React.createClass({
-    getInitialState: function() {
-      return {
-        markers: [{
-      position: {
-        lat: 25.0112183,
-        lng: 121.52067570000001,
+    var PlotMap = React.createClass({
+      getInitialState: function() {
+        return {
+          crumbs: [{
+            position: {
+              lat: 25.0112183,
+              lng: 121.5206757000001,
+            },
+            key: `Montreal`,
+            defaultAnimation: 2,
+          }]
+        };
       },
-      key: `Montreal`,
-      defaultAnimation: 2,
-    }]
-    };
-    },
-    
-  componentDidMount: function() {
-    if (!canUseDOM) {
-      return;
-    }
-    window.addEventListener(`resize`, this.handleWindowResize);
-  },
-  componentWillUnmount: function() {
-    if (!canUseDOM) {
-      return;
-    }
-    window.removeEventListener('resize', this.handleWindowResize);
-  }, 
-  
-  handleMapClick: function(e) {
-    let { markers } = this.state;
-    markers = update(markers, {
-      $push: [
-        {
-          position: {lat: e.latLng.lat(), lng: e.latLng.lng()},
-          key: Date.now()
-        },
-      ],
-    });
-    this.setState({ markers });
-    
-    /*if (markers.length === 3) {
-      this.props.toast(
-        `Right click on the marker to remove it`,
-        `Also check the code!`
-      );
-    }*/
-  },
-  handleMarkerRightclick: function(i,e) {
-    let { markers } = this.state;
-    markers = update(markers, {
-      $splice: [
-        [i, 1],
-      ],
-    });
-    this.setState({ markers });
-  },
-  render: function() {
-    return (
-      <GoogleMapLoader
-        containerElement={
-          <div
-            {...this.props}
-            style={{
-              height: `100vh`,
-            }}
+
+      componentDidMount: function() {
+        if (!canUseDOM) {
+          return;
+        }
+        window.addEventListener(`resize`, this.handleWindowResize);
+      },
+      componentWillUnmount: function() {
+        if (!canUseDOM) {
+          return;
+        }
+        window.removeEventListener('resize', this.handleWindowResize);
+      },
+
+      handleMapClick: function(e) {
+        let {
+          crumbs
+        } = this.state;
+        crumbs = update(crumbs, {
+          $push: [{
+            position: {
+              lat: e.latLng.lat(),
+              lng: e.latLng.lng()
+            },
+            key: Date.now()
+          }, ],
+        });
+        this.setState({
+          crumbs
+        });
+
+        /*if (crumbs.length === 3) {
+          this.props.toast(
+            `Right click on the crumb to remove it`,
+            `Also check the code!`
+          );
+        }*/
+      },
+      handleCrumbRightclick: function(i, e) {
+        let {
+          crumbs
+        } = this.state;
+        crumbs = update(crumbs, {
+          $splice: [
+            [i, 1],
+          ],
+        });
+        this.setState({
+          crumbs
+        });
+      },
+      render: function() {
+        return ( < GoogleMapLoader containerElement = { < div {...this.props
+            }
+            style = {
+              {
+                height: `100vh`,
+              }
+            }
+            />
+          }
+          googleMapElement = {
+            <GoogleMap
+                ref={(map) => (this._googleMapComponent = map) && console.log(map.getZoom())}
+                defaultZoom={16}
+                defaultCenter={{ lat: 45.5088400, lng: -73.5878100 }}
+                onClick={this.handleMapClick}
+                
+              >
+                {this.state.crumbs.map((crumb, index) => {
+                  return (
+                    <Marker
+                      {...crumb}
+                      onRightclick={this.handleCrumbRightclick.bind(this, index)}
+                    />
+                  );
+                })}
+              </GoogleMap>
+          }
           />
+        );
+      }
+    });
+
+    var Home = React.createClass({
+
+      render: function() {
+        return <p>HOME</p>
+      }
+    });
+    var PathList = React.createClass({
+
+      render: function() {
+        return <p>Path List</p>
+      }
+    });
+    var PathView = React.createClass({
+
+      render: function() {
+        return <p>Path View</p>
+      }
+    });
+
+    var Title = React.createClass({
+
+      render: function() {
+        return (
+          <form action="create">
+    <input type="text" name="pathTitle" ref= "title" placeholder="Name your path"/>
+    </form>
+        );
+      }
+    });
+
+    var Create = React.createClass({
+
+      handleClick: function(e) {
+        e.preventDefault()
+        var path = {
+          title: this.refs.title,
+          points: []
         }
-        googleMapElement={
-          <GoogleMap
-            ref={(map) => (this._googleMapComponent = map) && console.log(map.getZoom())}
-            defaultZoom={3}
-            defaultCenter={{ lat: -25.363882, lng: 131.044922 }}
-            onClick={this.handleMapClick}
-            
-          >
-            {this.state.markers.map((marker, index) => {
-              return (
-                <Marker
-                  {...marker}
-                  onRightclick={this.handleMarkerRightclick.bind(this, index)}
-                />
-              );
-            })}
-          </GoogleMap>
-        }
-      />
-    );
-  }
-  
-  
-})
+        console.log(path);
+        browserHistory.push("/list");
 
-var Home = React.createClass({
-  
-  render: function() {
-    return <p>HOME</p>
-  }
-})
-var PathList = React.createClass({
-  
-  render: function() {
-    return <p>Path List</p>
-  }
-})
-var PathView = React.createClass({
-  
-  render: function() {
-    return <p>Path View</p>
-  }
-  
-  
-})
+      },
 
+      render: function() {
+        return (
+          <button onClick={this.handleClick}>Create</button>
+        );
+      }
+    });
 
+    var PlotPage = React.createClass({
 
+      render: function() {
+        return (
+          <div>
+           <Title/>
+           <PlotMap/>
+           <Create/>
+         </div>
+        )
+      }
+    });
 
-// not found "page"
-var NotFound = React.createClass({
-  render: function() {
-    return (
-      <div>Not Found!</div>
-    );
-  }
-});
-/*
-The routes. This section says:
-  - If the route starts with /, load the App component
-  - If the route IS /, load the Home component INSIDE App as this.props.children
-  - If the route is /about, load the About component INSIDE App as this.props.children
-  - If the route is /team, load the Team component INSIDE App as this.props.children
-  - If the route is /about, load the About component INSIDE App as this.props.children
-  - If the route is anything else, load the NotFound component INSIDE App as this.props.children
-The whole process lets us create **complex, nested user interfaces** with minimal effort,
-by simply nesting `Route` components.
-*/
-var routes = (
-  <Router history={browserHistory}>
+    // not found "page"
+    var NotFound = React.createClass({
+      render: function() {
+        return (
+          <div>Not Found!</div>
+        );
+      }
+    });
+    /*
+    The routes. This section says:
+      - If the route starts with /, load the App component
+      - If the route IS /, load the Home component INSIDE App as this.props.children
+      - If the route is /about, load the About component INSIDE App as this.props.children
+      - If the route is /team, load the Team component INSIDE App as this.props.children
+      - If the route is /about, load the About component INSIDE App as this.props.children
+      - If the route is anything else, load the NotFound component INSIDE App as this.props.children
+    The whole process lets us create **complex, nested user interfaces** with minimal effort,
+    by simply nesting `Route` components.
+    */
+    var routes = (
+      <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Home}/>
-      <Route path="create" component={PlotMap}/>
+      <Route path="create" component={PlotPage}/>
       <Route path="list" component={PathList}/>
       <Route path="view/:id" component={PathView}/>
       <Route path="*" component={NotFound}/>
     </Route>
   </Router>
-);
-// If this line of code is not here, nothing gets displayed!
-ReactDOM.render(routes, document.querySelector('#app'));
+    );
+    // If this line of code is not here, nothing gets displayed!
+    ReactDOM.render(routes, document.querySelector('#app'));
